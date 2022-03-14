d9ef8c26d891e005b7bb01741e51810e0af84b59
isrardawar <isrardawar485@gmail.com>
2022-03-14 22:16:14 +0500

    GoPractice

---
+++ Go/GoPrograming/practice.go
@@ -0,0 +1,36 @@
+package main
+
+//task 1
+// func main() {
+// 	var name string = "israr"
+// 	fmt.Println("your name is: ",name)
+
+// 	var age int64 = 23
+// 	fmt.Println("your age is: ",age)
+
+// 	var height float64 = 6.3
+// 	fmt.Println("your height is: ",height)
+// }
+
+// main function
+//task 2
+// func main() {
+
+// 	// Println function is used to
+// 	// display output in the next line
+// 	fmt.Println("Enter Your age: ")
+// 	var age string
+// 	fmt.Scanln(&age)
+// 	fmt.Println("Your Age is: ", age)
+
+// 	fmt.Println("Enter Your height: ")
+// 	var height float64
+// 	fmt.Scanln(&height)
+// 	fmt.Println("Your height is: ", height)
+
+// 	fmt.Println("Enter Your weight: ")
+// 	var weight float64
+// 	fmt.Scanln(&weight)
+// 	fmt.Println("Your weight is: ", weight)
+
+// }
